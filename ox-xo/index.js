b91513e3f0663cad7fc548ab5973ad27fb0d1c5e
Aidana777 <aidananiazalieva27@gmail.com>
2022-01-10 13:42:18 +0600

    push

--- ox-xo/index.js
+++ ox-xo/index.js
@@ -1,10 +1,17 @@
-const btn=document.getElementById("btn")
-const box=document.getElementById("box")
-const boxbtn=document.getElementById("box-btn")
-btn.addEventListener("click",function() {
-    box.style.display= "block"
-})
+const btn = document.getElementById("btn");
+const box = document.getElementById("box");
+const boxbtn = document.getElementById("box-btn");
+btn.addEventListener("click", function () {
+    box.style.display = "block";
+});
 
-boxbtn.addEventListener("click",function() {
-    box.style.display= "none"
-})
+boxbtn.addEventListener("click", function () {
+    box.style.display = "none";
+});
+$(".gallery-list").magnificPopup({
+  delegate: "a",
+  type: "image",
+  gallery: {
+    enabled: true
+  }
+});
